13de1446ccfa205fb37c60002af8c0b4cf4a8cb9
nzws / ねじわさ <github@nzws.me>
2018-08-22 01:27:36 +0900

    Update bot.go

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -347,7 +347,7 @@ func playSound(play *Play, vc *discordgo.VoiceConnection) (err error) {
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	log.Info("Recieved READY payload")
-	s.UpdateStatus(0, "airhornbot.com")
+	s.UpdateStatus(0, "!knzk")
 }
 
 func scontains(key string, options ...string) bool {
